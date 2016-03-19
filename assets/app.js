2c454e55cfd97e5af39c867b3b82b365c66cda7f
Simone Carella <simo@simo.local>
2016-03-19 21:03:55 +0100

    build

--- assets/app.js
+++ assets/app.js
@@ -59319,7 +59319,7 @@
 	//   console.log('z', posZ)
 	// })
 
-	audio.currentTime = 60;
+	//audio.currentTime = -60
 	function getDistance(time) {
 	  var t = time / 1000;
 	  var distX = 1 * t + velocityX * Math.pow(t, 2) / 2;
